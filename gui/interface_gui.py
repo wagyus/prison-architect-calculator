24e56b3abdcce67ff168224fe8bc1ff068d73960
chrismelnyk <xplowersu@gmail.com>
2019-03-29 12:16:56 -0400

    added kitchen gui button

--- gui/interface_gui.py
+++ gui/interface_gui.py
@@ -4,20 +4,26 @@ from subprocess import call
 
 laundry = 'laundry_gui.py'
 workshop = 'workshop_gui.py'
+kitchen = 'kitchen_canteen_gui.py'
+
 def callpy1():
     call(['python', laundry] )
 
 def callpy2():
     call(['python', workshop])
+def callpy3():
+    call(['python', workshop])
 
 root = tk.Tk()
 img = tk.Image("photo", file="p_icon.png")
 root.tk.call('wm','iconphoto', root._w, img)
-root.geometry('400x200+100+200')
+root.geometry('400x300')
 root.title('Prison Architect Calculator v0.1')
 l1 = tk.Label(text="Prison Architect Calculator v0.1", fg="black", bg="white")
+
 b1 = tk.Button(root, text='Laundry Calculator', command=callpy1, height=6, width=57).grid(row=0, column=1)
 b2 = tk.Button(root, text='Workshop Calculator', command=callpy2, height=6, width=57).grid(row=1, column=1)
+b3 = tk.Button(root, text='Kitchen/Canteen Calculator', command=callpy3, height=6, width=57).grid(row=2, column=1)
 
 
 root.mainloop()
